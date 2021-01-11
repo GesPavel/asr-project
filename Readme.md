1e4bb529285aa5cf88712b8963644d76658c78cf
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2021-01-11 08:14:57 +0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,56 @@
+asr
+===
+
+*asr* is a simple renderer written in C++ for creative coding and data visualization purposes.
+
+## Prerequisites
+
+Install all the prerequisites first.
+
+* MSVC (with Latest Visual Studio), Clang (with Latest Xcode), GCC (any version with support of C++17)
+* CMake (version `3.17.0` or higher)
+* Conan (version `1.32.1` or higher)
+* GPU drivers (latest version with stable support of OpenGL ES 2.0)
+
+## Building
+
+Create a `build` directory and set it as a current working directory:
+
+```bash
+mkdir build
+cd build
+```
+
+Install dependencies with the Conan package manager once:
+
+```bash
+conan profile new default --detect # ignore error messages about existing profiles
+conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-conan"
+conan install .. --build sdl2
+```
+
+Configure the project to generate IDE or Makefiles:
+
+```bash
+# On Windows for Visual Studio
+cmake .. -G "Visual Studio 16 2019"
+
+# On macOS for Xcode
+cmake .. -G "Xcode"
+
+# on macOS or GNU/Linux to generate Makefiles
+cmake .. -G "Unix Makefiles"
+```
+
+Build the project:
+
+```bash
+cmake --build .
+```
+
+Run the test program from the `asr/build/bin/` directory:
+
+```bash
+cd .. # Ensure that the current working directory is set to the root asr folder.
+./build/bin/<name of the graphics test executable>
+```
---
+++ tests/first_triangle_test.cpp
@@ -0,0 +1,68 @@
+#include "asr.h"
+
+static const char Vertex_Shader_Source[] = R"(
+    #version 110
+
+    attribute vec4 position;
+    attribute vec4 color;
+
+    uniform float time;
+
+    varying vec4 fragment_color;
+
+    void main()
+    {
+        fragment_color = color;
+
+        vec4 rotated_position = position;
+        rotated_position.x = position.x * cos(time) - position.y * sin(time);
+        rotated_position.y = position.x * sin(time) + position.y * cos(time);
+
+        gl_Position = rotated_position;
+    }
+)";
+
+static const char Fragment_Shader_Source[] = R"(
+    #version 110
+
+    varying vec4 fragment_color;
+
+    void main()
+    {
+        gl_FragColor = fragment_color;
+    }
+)";
+
+static const float Triangle_Geometry_Data[] = {
+//   Position       Color (RGBA)
+    -0.5f, -0.305f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+     0.0f,  0.565f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+     0.5f, -0.305f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+};
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
+{
+    create_es2_sdl_window();
+    create_es2_shader_program(
+        Vertex_Shader_Source,
+        Fragment_Shader_Source
+    );
+    generate_es2_geometry(
+        Triangle_Geometry_Data,
+        sizeof(Triangle_Geometry_Data)
+    );
+
+    prepare_for_es2_rendering();
+
+    bool should_stop = false;
+    while (!should_stop) {
+        process_es2_sdl_window_events(&should_stop);
+        render_next_es2_frame();
+    }
+
+    destroy_es2_geometry();
+    destroy_es2_shader_program();
+    destroy_es2_sdl_window();
+
+    return 0;
+}
